021e336cf7e3ce144b7ece3b7e1dfa7825600fa3
Gareth Evans <g.evans@sap.com>
2022-01-28 16:19:53 +0000

    Adding commands in root func

--- cmd/root.go
+++ cmd/root.go
@@ -9,21 +9,16 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "maven-resource",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "Implementation of a concourse resource that queries maven dependencies",
+	Long:  `Implementation of a concourse resource that queries maven dependencies`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
+	rootCmd.AddCommand(NewInCmd().Command)
+	rootCmd.AddCommand(NewCheckCmd().Command)
+
 	err := rootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
